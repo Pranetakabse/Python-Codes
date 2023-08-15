eff023e8e293cd057ac03be1be87352d9b317c26
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 02:08:46 -0400

    List and Tuple modifications

---
+++ day11.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[:5])
+print(thislist[3:])
+
+print(thislist[-4:-1])
+
+
+
+######Add string with integer
+
+print("cookies" + "2")
