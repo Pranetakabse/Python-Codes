deee8a0884d2e80acb623b8e026bce9014f74387
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 00:54:21 -0400

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,73 @@
+### create a variable and assign a value to it.
+
+### Introduce Yourself
+
+print("Hi! My name is Praneta.")
+
+### TO Quote a quote
+
+print("\"First, solve the problem.\" - Praneta")
+
+
+### Mood is variable
+
+mood = "curious"
+print(f"Today, I feel {mood}!")
+
+###Multiple line
+
+lines = """
+Hi, I am Praneta.
+I am happy today
+I like to learn Python.
+"""
+
+print(lines)
+# print (f"{lines}")
+
+
+### STORY
+
+name = "Neha"
+adjective = "smart"
+favorite_snack = "cookies"
+number = "3"
+type_of_tree = "mango"
+
+silly_story = f"""
+Hi, my name is {name}.
+I really like {adjective} {favorite_snack}!
+I try to eat it at least {number} times every day.
+It tastes even better when you eat it under a {type_of_tree}!
+"""
+
+print(silly_story)
+
+
+### Introduce name and age
+name = "Praneta"
+age = 20 + 7
+
+print(f" Hi! My name is {name} and I am {age} years old!")
+
+##### Strings and operations
+
+print("basket" + "ball")
+
+first_name = "Praneta"
+last_name = " ABC"
+
+print(first_name + last_name)
+
+print(first_name + " " + last_name)
+print(first_name + last_name)
+
+### A confused Computer
+
+#print( 3 + "cookies")
+
+### Multiplying Strings
+
+print( 3 * " balloon!")
+
+
