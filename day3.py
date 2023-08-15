be01b6658625d6156f704b49189d63f91770e1a3
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 00:53:02 -0400

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,101 @@
+total = 20 + (20 * 25) - 1.5 + 3
+print(total)
+
+print(2**4)
+
+print(4*3/2)
+
+total = 20 + 1.65 - 1.5 +3
+print(total)
+
+## Comparison operator 
+
+#1) Greater than ( >)
+
+a = 3
+b = 7
+
+print( a > b )
+
+# 2) Less Than (<)
+
+a = 2
+b = 4
+
+print( a < b)
+
+# 3) Greater Than or equal to (>=)
+
+a = 2
+b = 1
+
+print( a >= b)
+
+#4) Less Than or equal to ( <=)
+
+a = 5
+b = 1
+
+print( a <= b)
+
+#5) Equal to ( == )
+
+a = 4
+b = 4
+
+print( a == b )
+
+#6) Not equal to ( != )
+
+a = 3
+b = 4
+
+print ( a != b)
+
+
+### Logical Operands.
+# 1) and operator ( and )
+
+x = 5
+
+print( x > 3 and x < 10)
+
+
+#2) or operator ( or )
+
+x = 5
+print ( x > 3 or x< 3 )
+
+x = 10
+print ( x > 10 or x > 2)
+
+
+
+# not operator ( not)
+
+x = 5
+
+print( not (x > 3 and x < 10))
+
+
+
+####### Not eual to eg. )
+
+
+
+print ( 5 !=  "five")
+
+print( 10 != "10")
+
+
+print( 4 != 3)
+
+print ( 9 != 9 )
+
+
+
+
+
+
+
+
