01234804076db526d755f3255d73c9db673153b0
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 02:05:01 -0400

    Python Test Questions

---
+++ Day10_Python_test1_questions.py
@@ -0,0 +1,36 @@
+''
+
+Q.1 ) Create a list containing string, int and boolean data types and print
+the list.
+
+
+Q.2)write list with duplicate values
+
+Q.3)Print the number of items in the list.
+
+Q.4) Print data type of a list. 
+
+Q.5) add an item into the list.
+
+Q.6) remove an item from the list.
+
+Q.7) del an item from the list
+
+Q.8)slice the list.
+
+------------------------------------------------
+
+Q.9) Create a tuple
+
+Q.10) write length of tuple
+
+Q.11) print data type of tuple
+-----------------------------------------------
+
+Q.12) add two stings using + operator
+
+Q.13) multiply a string with a number
+
+Q.14) add a number and a string
+
+------------------------------------------------
