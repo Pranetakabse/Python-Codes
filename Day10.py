2178a092ed93ab411652c2a0f099f0122c6dba75
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 02:04:08 -0400

    Example of List

---
+++ Day10.py
@@ -0,0 +1,22 @@
+#### LIST ####
+
+your_cloud_shapes = []
+friend_cloud_shapes = []
+
+your_cloud_shapes = ['circle', 'turtle', 'dolphin', 'truck', 'apple', 'spoon']
+friend_cloud_shapes = ['apple', 'turtle', 'spoon', 'truck', 'circle', 'dolphin']
+
+if your_cloud_shapes[0] == friend_cloud_shapes[0]:
+    print("We saw the same shape!")
+
+elif your_cloud_shapes[0] != friend_cloud_shapes[0]:
+    print("We saw different shapes this time.")
+
+
+if your_cloud_shapes[1] == friend_cloud_shapes[1]:
+    print("We saw the same shape!")
+
+elif your_cloud_shapes[1] != friend_cloud_shapes[1]:
+    print("We saw the different shapes this time.")
+
+    
