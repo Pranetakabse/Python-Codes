d5c8038e8b2b90b3790039340651d930e694a715
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 00:55:08 -0400

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,75 @@
+### LIST
+my_favorite_desserts = ['cookies', 'cake', 'Ice Cream', 'Donuts'] # list of strings
+print(my_favorite_desserts)
+
+
+#list of integers
+
+bunnies_spotted = [3, 5, 2, 8, 5, 4 ]
+print(bunnies_spotted)
+
+#list of Booleans
+
+robot_answers = [True, False, False, True, True]
+print(robot_answers)
+### mix of string, integers, booleans
+
+mix_of_all = ['tree', 4, True, 'cars', 'books', 5]
+print(mix_of_all)
+
+### Lists are ordered
+
+citrus_fruits = ['orange', 'lemon', 'grapefruit', 'lime']
+more_citrus_fruits = ['orage', 'grapefruit', 'lime', 'lemon']
+
+fruits = citrus_fruits == more_citrus_fruits
+
+print(fruits)
+
+
+
+### Lists can be accessed with index
+
+citrus_fruits = ['orange', 'lemon', 'grapefruit', 'lime']
+
+print(citrus_fruits[2])
+
+print(citrus_fruits[0])
+
+##List can be sliced
+
+
+
+citrus_fruits = ['orange', 'lemon', 'grapefruit', 'lime']
+
+print(citrus_fruits[1:3])
+
+print(citrus_fruits[0:2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
