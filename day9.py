32aae9fdff902d42a9bc6982b1e88eda1fd4afcc
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 02:02:40 -0400

    Dealing with Errors and Exceptions

---
+++ day9.py
@@ -0,0 +1,7 @@
+#### Create a list named my_favorite_things and add 5 things to it.
+## print - "These are {your name}'s favorite things:[' ', ' ', ]
+## f - string
+
+my_favorite_things = ['Blue', 3 , 'Desserts','Running', 33.3]
+
+print(f"These are Praneta's favorite things: {my_favorite_things}")
