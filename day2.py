b9bde53470f9c8243babae425028c5b604a4c77f
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 00:52:06 -0400

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,74 @@
+#100_days_of_code = 100
+numberOfCookies = 3
+NumberOfCookies = 5
+number_of_cookies = 4
+
+
+## eg. of good variables
+mood = "happy"
+age = 10
+favourite_color = "purple"
+
+## eg. of not so good variables
+
+a = 5
+curDy = "Thursday"
+fAvOrIte_DrInk = "coffee"
+
+## Fancy printing
+## formatted string literals
+
+print("I'm so happy to be larning Python!")
+
+
+food = "cake"
+print(f"I like {food}")
+
+feeling = "sad"
+print(f"I'm so {feeling} to learn Python!")
+
+##Multi-line string
+
+print("Here is \na sentence \non many \ndifferent lines.")
+
+multiline_sentence = """
+Here is
+a sentence
+on many
+different lines.
+"""
+print(f"{multiline_sentence}")
+## float
+
+
+my_gpa = 3.47
+
+a = 12
+b = 5
+
+print(a + b) # addtition
+print(a - b) # subtraction
+print(a * b) # multiplication
+print( a / b) # division
+
+## modulus (%) - divides one value by another, and returns the remainder.
+print(a % b)
+
+## floor Division (//) - divides one value by another, returns the answer to the next
+## smallest whole number
+print (a // b)
+
+## exponentiation ( **) - raises one value to the power of another
+print ( 2**5) 
+
+
+
+
+
+
+
+
+
+
+
+
