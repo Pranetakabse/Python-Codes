2bb600e8b0bef83aa4205be0f662f1e870777bc0
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-08-15 00:50:37 -0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,35 @@
+
+
+# print("I shoukd not be printed")
+
+print("Hello")
+# print("You are here")
+
+print("I'm so happy to be learning how to write a code") 
+      
+
+
+## Escape Character
+print('I\'m happy to be learning how to write a code')
+
+print("\"Kumusta\" is \"Hello\" in Tagalog!")
+
+## multiple lines
+print("Here is \na sentence \non many \ndifferent lines.")
+
+
+## variable 
+
+name = "praneta"
+print(name)
+
+print(name)
+
+favorite_number = "3"
+
+
+type(favorite_number)
+
+## type
+x = "10"
+print(type(x))
