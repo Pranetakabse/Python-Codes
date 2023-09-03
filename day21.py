eb6ef7d634b7455fca6752f82a1864bba8a0881c
Praneta <94325978+Pranetakabse@users.noreply.github.com>
2023-09-02 20:59:11 -0400

    Test 2 Question

---
+++ day21.py
@@ -0,0 +1,24 @@
+###### Test questions
+'''
+1) Create a class named Person, with firstname and lastname properties,
+and a printname method.
+
+
+2)Create a class named Person, with firstname and lastname properties,
+and a printname method. Use the Student class to create an object, and
+then execute the printname method.
+
+3)Create a class named Person, with firstname and lastname properties,
+and a printname method. Use the Student class to create an object, and
+then execute the printname method. Use the super() function and a property
+to the child class.
+
+4) create a class and object. First delete the Object property and
+then delete the object.
+
+5) Create a class with __str__()  function.
+
+'''
+
+
+
